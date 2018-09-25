cb49c5e623f773b645a98b57d4e08f7feb2bdfeb
HarshSharma12 <mail.hs.harsh@gmail.com>
2018-09-25 03:30:19 -0600

    Assignment 1

---
+++ MM806-2018-1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int score;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        score = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("LowValuePickUp"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("HighValuePickUp"))
+        {
+            other.gameObject.SetActive(false);
+            score+=2;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Current Score: " + score.ToString();
+        if (score >= 10)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
